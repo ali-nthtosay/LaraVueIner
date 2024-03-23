bf831a79346252b01f1a43db5e2106789d36fda1
Amidpour <98856912+ali-nthtosay@users.noreply.github.com>
2024-03-24 00:02:01 +0100

    editing

--- resources/js/Layouts/MainLayoutPage.vue
+++ resources/js/Layouts/MainLayoutPage.vue
@@ -3,7 +3,7 @@
     <nav class="bg-gray-300 border-gray-200 dark:bg-gray-900 mb-5">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/dashboard/listing" class="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="../Components/Images/easimo-Logo.png" class="h-5" alt="Logo" />
+          <img src="" class="h-5" alt="Logo" />
           <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{{ user.name }}</span>
         </a>
         <button
