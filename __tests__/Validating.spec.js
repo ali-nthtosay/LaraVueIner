d800d5b81219b62cf6b42b16b7f9b38e6186bf1d
shadowahad <abdulahadtahir433@gmail.com>
2024-03-27 21:00:05 +0500

    .

--- __tests__/Validating.spec.js
+++ __tests__/Validating.spec.js
@@ -11,9 +11,3 @@ test("Check Password valid", () => {
 test("Check Email valid", () => {
     expect(!isValidEmail(email)).toBe(false);
 });
-
-// Check if the password is valid:
-// This test case aims to verify whether the provided password meets the required criteria for validity. The isPasswordValid function is assessed to ensure it correctly validates the format of the password according to predefined rules. By executing this test, we confirm that the utility function behaves as expected, accurately determining the validity of passwords based on specified criteria.
-
-// Check if the email is valid:
-// This test case focuses on determining the validity of a given email address according to standard email format rules. The isValidEmail function is evaluated to validate its ability to accurately assess the correctness of email addresses. Executing this test ensures that the utility function correctly identifies valid email addresses, thereby enhancing the reliability of email-related functionalities in the application.
